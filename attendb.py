ecd0faa713582f6d1b0395e75132957c118cd09b
Furao <vanvossr@gmail.com>
2011-08-31 00:20:47 -0400

    Able to add student for attendance and changed to only connect to database during queries

--- attendb.py
+++ attendb.py
@@ -1,63 +1,74 @@
 #!/usr/bin/python
 import sqlite3
 
-class AttenDB():
+class AttendDB():
 
     def __init__(self):
         """Interaction with the sqlite database."""
         self.db_name = 'Attendance.db'
         self.class_table = 'academy'
 
+    def open_connection(self):
         self.conn = sqlite3.connect(self.db_name)
         self.c = self.conn.cursor()
 
     def add_student(self, apelido="", fn="", ln=""):
         """Add new student to table."""
+        self.open_connection()
         self.c.execute(\
             "INSERT INTO students (apelido, fn, ln) VALUES (?, ?, ?)",\
             (apelido, fn, ln))
         self.conn.commit()
+        self.close_connection()
 
     def remove_student(self, apelido="", fn="", ln=""):
         """Remove student from table."""
+        self.open_connection()
         self.c.execute(\
             "DELETE FROM students WHERE apelido=? AND fn=? AND ln=?",\
             (apelido, fn, ln))
         self.conn.commit()
+        self.close_connection()
 
-    def get_attendace_for_date(self, curr_date):
+    def get_attendance_for_date(self, curr_date):
         """Get the students that attended class on a given date from today."""
+        self.open_connection()
         students = []
-        self.c.execute("SELECT students.apelido FROM academy JOIN students " +\
+        self.c.execute("SELECT students.* FROM academy JOIN students " +\
                 "ON academy.Sid=students.Sid " + \
-                "WHERE academy.classDate='"+curr_date+"' ORDER BY " + \
-                "students.apelido")
+                "WHERE academy.classDate='"+str(curr_date)+"'")
         for row in self.c:
             students.append(row)
 
+        self.close_connection()
         return students
 
     def get_students(self):
+        self.open_connection()
         students = []
-        self.c.execute("SELECT * FROM students ORDER BY apelido, fn")
+        self.c.execute("SELECT * FROM students")
         for row in self.c:
             students.append(row)
 
+        self.close_connection()
         return students
 
     def student_attended(self, sid, curr_date):
         """Add students attendance to table."""
+        self.open_connection()
         self.c.execute(\
-            "INSERT INTO academy (Sid, classDate) VALUES (?, ?)",\
-            (sid, curr_date))
+            "INSERT INTO academy (Sid, classDate) VALUES ("+str(sid)+", '"+str(curr_date)+"')")
         self.conn.commit()
+        self.close_connection()
 
     def student_deattend(self, sid, curr_date):
         """Rmove student attendance from table."""
+        self.open_connection()
         self.c.execute(\
             "DELETE FROM academy WHERE Sid=? AND classDate=?",\
             (sid, curr_date))
         self.conn.commit()
+        self.close_connection()
 
     def close_connection(self):
         """Close the connection."""
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 from PyQt4 import QtCore, QtGui
 from mainwindow import Ui_MainWindow
-from attendb import AttenDB
+from attendb import AttendDB
 
 class Main(QtGui.QMainWindow):
     
@@ -15,19 +15,61 @@ class Main(QtGui.QMainWindow):
         self.ui.setupUi(self)
 
         # Set date to today
-        today = QtCore.QDate.currentDate()
-        self.ui.dateEdit.setDate(today)
+        self.date = QtCore.QDate.currentDate()
+        self.ui.dateEdit.setDate(self.date)
+
+        #self.dateString = self.date.toString("yyyy-MM-dd")
 
         # Create Attendance DB Connection Object
         self.db = AttendDB()
 
+        # Connect signals to handlers
+        self.connect(self.ui.addButton, QtCore.SIGNAL("clicked()"), self.on_student_attend)
+        self.connect(self.ui.dateEdit, QtCore.SIGNAL("dateChanged(const QDate&)"), self.on_date_change)
+
+        self.ui.attendListWidget.setSortingEnabled(True)
+        self.ui.availListWidget.setSortingEnabled(True)
+
+        self.update_views(self.date)
+
+    def on_date_change(self, date):
+        self.update_views(date)
+
+    def update_views(self, date):
+    
+        self.date = date
+        self.dateString = date.toString("yyyy-MM-dd")
+
+        self.curr_students = self.db.get_attendance_for_date(self.dateString)
+
+        self.ui.attendListWidget.clear()
+        self.ui.availListWidget.clear()
+
+        for item in self.curr_students:
+            if item[1] != None:
+                self.ui.attendListWidget.addItem(str(item[1]))
+            else:
+                self.ui.attendListWidget.addItem(str(item[2]) + ' ' + str(item[3]))
+
+        self.student_dict = {}
         # Get all students
         self.students = self.db.get_students()
-        for item in student:
-            print item
+        for item in self.students:
+            if item not in self.curr_students:
+                if item[1] != None:
+                    self.ui.availListWidget.addItem(str(item[1]))
+                    self.student_dict[str(item[1])] = item[0]
+                else:
+                    self.ui.availListWidget.addItem(str(item[2]) + ' ' + str(item[3]))
+                    self.student_dict[str(item[2]) + ' ' + str(item[3])] = item[0]
 
-    def on_addButton_clicked(self):
-        print 'Add Button Pressed'
+    def on_student_attend(self):
+        ci = self.ui.availListWidget.currentItem() 
+        print ci.text()
+        print self.student_dict[str(ci.text())]
+        print self.dateString
+        self.db.student_attended(self.student_dict[str(ci.text())], self.dateString)
+        self.update_views(self.date)
 
     def on_removeButton_clicked(self):
         print 'Remove Button Pressed'
