c017488d8e5494ad29b7b8b653cafdc9289a04a4
Furao <vanvossr@gmail.com>
2011-08-31 14:12:24 -0400

    Added removing student from current date attendance

--- main.py
+++ main.py
@@ -18,61 +18,109 @@ class Main(QtGui.QMainWindow):
         self.date = QtCore.QDate.currentDate()
         self.ui.dateEdit.setDate(self.date)
 
-        #self.dateString = self.date.toString("yyyy-MM-dd")
+        # Date format for SQLite
+        self.date_format = "yyyy-MM-dd"
 
+        # Used to remember the Students Id
+        self.student_dict = {}
+        
         # Create Attendance DB Connection Object
         self.db = AttendDB()
 
         # Connect signals to handlers
-        self.connect(self.ui.addButton, QtCore.SIGNAL("clicked()"), self.on_student_attend)
-        self.connect(self.ui.dateEdit, QtCore.SIGNAL("dateChanged(const QDate&)"), self.on_date_change)
+        self.connect(self.ui.addButton, QtCore.SIGNAL("clicked()"), \
+                self.on_add_clicked)
+        self.connect(self.ui.removeButton, QtCore.SIGNAL("clicked()"), \
+                self.on_remove_clicked)
+        self.connect(self.ui.dateEdit, \
+                QtCore.SIGNAL("dateChanged(const QDate&)"), \
+                self.on_date_change)
 
+        # Enable sorting on the list widgets
         self.ui.attendListWidget.setSortingEnabled(True)
         self.ui.availListWidget.setSortingEnabled(True)
 
+        # Update the lists to reflect the current date
         self.update_views(self.date)
 
     def on_date_change(self, date):
+        """Update views when the date is changed."""
         self.update_views(date)
 
     def update_views(self, date):
-    
+        """Refresh the students available and attending for the given date."""
         self.date = date
-        self.dateString = date.toString("yyyy-MM-dd")
 
-        self.curr_students = self.db.get_attendance_for_date(self.dateString)
+        # Get correct date format
+        self.date_string = date.toString(self.date_format)
 
+        # Get student attendance for the given date
+        self.curr_students = self.db.get_attendance_for_date(self.date_string)
+
+        # Clear the items in the list
         self.ui.attendListWidget.clear()
         self.ui.availListWidget.clear()
 
+        # Check to see if a student has a nickname
+        # If not, use their first and last name
         for item in self.curr_students:
             if item[1] != None:
-                self.ui.attendListWidget.addItem(str(item[1]))
+                name = str(item[1])
             else:
-                self.ui.attendListWidget.addItem(str(item[2]) + ' ' + str(item[3]))
-
-        self.student_dict = {}
+                name = str(item[2]) + ' ' + str(item[3])
+            # Add student to the attended list view
+            self.ui.attendListWidget.addItem(name)
+        
         # Get all students
         self.students = self.db.get_students()
+
+        # Check to see if a student has a nickname
+        # If not, use their first and last name
         for item in self.students:
+            # Dont add them to available list if they attended given date
             if item not in self.curr_students:
                 if item[1] != None:
-                    self.ui.availListWidget.addItem(str(item[1]))
-                    self.student_dict[str(item[1])] = item[0]
+                    name = str(item[1])
                 else:
-                    self.ui.availListWidget.addItem(str(item[2]) + ' ' + str(item[3]))
-                    self.student_dict[str(item[2]) + ' ' + str(item[3])] = item[0]
+                    name = str(item[2]) + ' ' + str(item[3])
+                # Add student to available list view
+                self.ui.availListWidget.addItem(name)
 
-    def on_student_attend(self):
-        ci = self.ui.availListWidget.currentItem() 
-        print ci.text()
-        print self.student_dict[str(ci.text())]
-        print self.dateString
-        self.db.student_attended(self.student_dict[str(ci.text())], self.dateString)
-        self.update_views(self.date)
+            # Add student and Sid to student dictionary
+            self.student_dict[name] = item[0]
 
-    def on_removeButton_clicked(self):
-        print 'Remove Button Pressed'
+    def on_add_clicked(self):
+        """Move student from available to attended list."""
+        # Get selected item in list
+        ci = self.ui.availListWidget.currentItem()
+        # Convert to string
+        curr_student =  str(ci.text())
+        try:
+            # Actually add the student for the date into the database
+            self.db.student_attend(self.student_dict[curr_student], \
+                    self.date_string)
+            self.update_views(self.date)
+        except KeyError:
+            # Display error window if student missing
+            err_msg = QtGui.QErrorMessage()
+            err_msg.showMessage("Sid nto found for student %s" % curr_student) 
+
+
+    def on_remove_clicked(self):
+        """Move student from attended to available list."""
+        # Get selected item in list
+        ci = self.ui.attendListWidget.currentItem()
+        # Convert to string
+        curr_student =  str(ci.text())
+        try:
+            # Actually remove the student for the date from the database
+            self.db.student_deattend(self.student_dict[curr_student], \
+                    self.date_string)
+            self.update_views(self.date)
+        except KeyError:
+            # Display error window if student missing
+            err_msg = QtGui.QErrorMessage()
+            err_msg.showMessage("Sid nto found for student %s" % curr_student) 
     
 def main():
     app = QtGui.QApplication(sys.argv)
